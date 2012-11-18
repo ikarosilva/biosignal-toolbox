042457d984013fa3ed6e52fb24fa63d522ad4d5c
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2012-11-18 02:19:05 +0000

    adding function signatures for nonlinear analysis

---
+++ BiosignalToolbox/src/com/ikarosilva/nonlinear/SurrogateData.java
@@ -0,0 +1,15 @@
+package com.ikarosilva.nonlinear;
+
+public class SurrogateData {
+	
+	//Methods for generating surrogate data based on :
+   //Understanding Nonlinear Dynamics, Kaplan and Glass, 1995
+	
+	public static void fftShuffle(){
+		//TODO: implement method in pg 343 and 354
+	}
+
+	public static void ampAdjustedFFTShuffle(){
+		//TODO: implement method in pg 356
+	}
+}
