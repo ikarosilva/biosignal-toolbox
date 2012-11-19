25325240e36f8ea760920b2896866440cf910617
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2012-11-19 04:26:07 +0000

    adding files for acoustic processing

--- BiosignalToolbox/src/com/ikarosilva/graphics/SpectralPlottingDemo1.java
+++ BiosignalToolbox/src/com/ikarosilva/graphics/SpectralPlottingDemo1.java
@@ -1,6 +1,11 @@
 
     package com.ikarosilva.graphics;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import javax.swing.JPanel;
 
 import org.apache.commons.math3.complex.Complex;
@@ -50,16 +55,17 @@ import org.jfree.ui.RefineryUtilities;
         	XYSeriesCollection result = new XYSeriesCollection();
         	XYSeries series = new XYSeries(1);
         	int N =1024*4;
-        	double Fs=10;
-        	double F=3;
-    		double[] a = new double[N];
-    		for(int n=0;n<N;n++){
-    			a[n]=Math.sin((double)2*Math.PI*F*n/Fs);
+        	double Fs=8000;
+        	double[] data=null;
+    		try {
+    			data = readBinary2("/home/ikaro/oae_data/silva2.raw");
+    		} catch (IOException e) {
+    			e.printStackTrace();
     		}
         	
     		//Calculate spectrum 
     		FastFourierTransformer FFT = new FastFourierTransformer(DftNormalization.STANDARD);
-    		Complex[] A= FFT.transform(a, TransformType.FORWARD);
+    		Complex[] A= FFT.transform(data, TransformType.FORWARD);
     		double fres= (double) Fs/N;
     		//System.out.println("res is " + fres);
     		for(int n=0;n<(N/2);n++){
@@ -81,5 +87,43 @@ import org.jfree.ui.RefineryUtilities;
             RefineryUtilities.centerFrameOnScreen(demo);
             demo.setVisible(true);
         }
+        
+        public static double[] readBinary2(String aInputFileName) throws IOException{
+    		
+    		//Check that data is ok
+    		FileInputStream nFile = null;
+    		
+    		try {
+    			nFile = new FileInputStream(aInputFileName);
+    		} catch (FileNotFoundException e2) {
+    			e2.printStackTrace();
+    		}	
+    		ObjectInputStream in = null;
+    		try {
+    			in = new ObjectInputStream(nFile);
+    		} catch (IOException e1) {
+    			e1.printStackTrace();
+    		}
+
+    		short[] tmpdata=null;
+    		try {
+    			try {
+    				tmpdata = (short[]) in.readObject();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		} catch (ClassNotFoundException e) {
+    			e.printStackTrace();
+    			System.out.print("Could not open data file.");
+    		}	
+    		in.close();
+    		int N=(int) Math.pow(2,Math.floor(Math.log((int) tmpdata.length)/Math.log(2)));
+    		double[] data=new double[N];
+    		for(int i=0;i<N;i++){
+    			data[i]=(double) tmpdata[i];
+    		}
+    		return data;
+    		
+    	}
 
     }
