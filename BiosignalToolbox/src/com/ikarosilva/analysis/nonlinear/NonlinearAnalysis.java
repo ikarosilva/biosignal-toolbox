92c98fc775e604982a0aefd8885f3355d6e2f4cb
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-03-13 19:12:58 +0000

    reorganizing

---
+++ BiosignalToolbox/src/com/ikarosilva/analysis/nonlinear/NonlinearAnalysis.java
@@ -0,0 +1,66 @@
+package com.ikarosilva.analysis.nonlinear;
+
+
+import java.io.IOException;
+import org.jfree.ui.RefineryUtilities;
+import com.ikarosilva.graphics.Plot;
+
+public class NonlinearAnalysis {
+
+	
+	public NonlinearAnalysis(double data){
+		
+	}
+	//Nonlinear analysis methods from :
+    //Understanding Nonlinear Dynamics, Kaplan and Glass, 1995
+	public static void returnPlot(double x0,double R, int N){
+		//TODO: implement return Map
+	}
+	
+	public static void corrIntegral(){
+		//TODO: implement method in pg 317 and 353
+		//Assumes that the time series came from a dynamical system that is on
+		//a attractor!!
+	}
+	
+	public static void corrDimension(){
+		//TODO implment method in 320 and 354
+	}
+	
+	public static void deterministicError(){
+		//TODO implment method in 325
+	}
+	
+	public static void linearModel(){
+		//TODO implment method in 337
+	}
+	
+	public static void lyapunov(){
+		//TODO implmente method in 335
+		//Assumes data is well described by deterministic 
+		//finite difference equation
+	}
+	
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		//Get directory listing 
+
+		int N=1000;
+	    double[] data= new double[N];
+	    //Simulate with a rnadom noise signal
+	    for(int n=0;n<N;n++){
+	    	data[n]=Math.random();
+	    }
+	    
+	    
+		Plot demo0 = new Plot("Signal",data);
+		demo0.pack();
+		RefineryUtilities.centerFrameOnScreen(demo0);
+		demo0.setVisible(true);
+		
+
+	}
+
+	
+}
