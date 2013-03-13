a1f64dea3e7f087f00fc6096813ff047f837139a
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-03-13 16:29:33 +0000

    renaming for clarity

--- BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
+++ BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
@@ -16,7 +16,7 @@ public class PdfEstimator {
 
 
 	public static double[][] equipartition(ArrayList<Double> data, int Nbins){
-		double[][] y = new double[Nbins][2];
+		double[][] y = new double[Nbins][3];
 		Collections.sort(data);
 		Double binWidth=(data.get(data.size()-1)-data.get(0))/Nbins;
 		int LB=0,UB;
@@ -31,10 +31,11 @@ public class PdfEstimator {
 			}else{
 				UB=data.size()-1;
 			}		
-			y[n][0]=data.get(UB);
-			y[n][1]=(double) (UB-LB)/data.size();
+			y[n][0]=data.get(LB);
+			y[n][1]=data.get(UB);
+			y[n][2]=(double) (UB-LB)/data.size();
 			LB=UB+1;
-			System.out.println(y[n][0] +"," + y[n][1] + "\n");
+			System.out.println(y[n][0] +"," + y[n][1] +","+ y[n][2] + "\n");
 		}
 		return y;
 	}
@@ -50,7 +51,7 @@ public class PdfEstimator {
 			x.add((double) Math.random());
 		}
 		hist=equipartition(x,10);
-		System.out.println("Entropy= " + InformationTheory.NormalizedEntropy(hist));
+		//System.out.println("Entropy= " + Entropy.NormalizedEntropy(hist));
 	}
 
 }
