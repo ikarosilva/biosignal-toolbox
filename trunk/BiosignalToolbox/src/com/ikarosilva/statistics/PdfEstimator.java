911ed187007c4378af59b58fdc8ff4840a0d0897
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-03-16 16:39:28 +0000

    generalized pdf to different dimensions.

--- trunk/BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
+++ trunk/BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
@@ -1,100 +1,96 @@
 package com.ikarosilva.statistics;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeMap;
-
-import org.jfree.ui.RefineryUtilities;
-
-import com.ikarosilva.graphics.Plot;
-
 public class PdfEstimator {
 
 
 	//px : 1 column= probability, 2nd column lower bound, 3rd column upper bound
 	private double[][] pdf;
 	private int Nbins;
+	private int M;
 
 	public PdfEstimator(){
 
 	}
 
+	private double[] minMax(double[] x){
+		//Returns minmax for a vector
+		double[] minMax=new double[2];
+		minMax[0]=Double.MAX_VALUE;
+		minMax[1]=Double.MIN_VALUE;
+		for(int i=0;i<x.length;i++){
+			minMax[0]=(minMax[0]>x[i]) ? x[i]:minMax[0];
+			minMax[1]=(minMax[1]<x[i]) ? x[i]:minMax[1];
+		}
+		return minMax;
+	}
 
-	public double[][] equipartition(ArrayList<Double> x, 
-			ArrayList<Double> y, int N){
+	public HashMap<Integer, Double> equipartition(double[][] x, int N){
 		Nbins=N;
-		//First is pdf, 2nd LB on x, 3rd UB on x
-		//4th LB on y, 5th UB on y
-		pdf = new double[Nbins*Nbins][5];
-		Double minX = Collections.min(x);
-		Double minY = Collections.min(y);
-		Double maxX = Collections.max(x);
-		Double maxY = Collections.max(y);
-		Double rangeX= maxX-minX;
-		Double rangeY= maxY-minY;
-		int nX, nY;
+		M=x.length;
+		int samples=x[0].length;
+		double wcount= (double) 1/samples;
+		double[][] minmax=new double[M][2];
+		double[] range=new double[M];
+		double[] scale= new double[M];
+		double[] step= new double[M];
+		double[] picketFence= new double[M];
+		double[] offset= new double[M];
+		int key; 
+		int[] rnd= new int[M];
+		int[] mapDim=new int[M];
+		Double tmp;
+		HashMap<Integer,Double> pdf=new HashMap<Integer,Double>();
+
+		System.out.println("M=" + M + " samps= " + samples);
+		//Initialize parameters for the the pdf
+		for(int i=0;i<M;i++)
+		{
+			minmax[i]=minMax(x[i]);
+			range[i]= minmax[i][1]-minmax[i][0];
+			scale[i]=(double)(Nbins-1)/range[i];
+			step[i]=(double)range[i]/Nbins;
+			picketFence[i]=(minmax[i][1]+step[i])/minmax[i][1];
+			offset[i]=(double)step[i]/2;
+			mapDim[i]=(int) Math.pow(Nbins,M-i-1);
+		}
 		//Create HashMap to store bin locations of X and Y
 		//then use the HashMap to add to the count
 		//The HashMap will be created by normalizing the variable and
 		//multiplying by the number of bins. So keys are in the 
-		// round(normalized*N) domain //TODO: ensure capacity ??
-		HashMap<Integer,Double> xy=new HashMap<Integer,Double>();	
-
-		//Initialize HashMap,
-		double scaleX=(N-1)/rangeX;
-		double scaleY=(N-1)/rangeY;
-		double stepX=(double)rangeX/N;
-		double stepY=(double)rangeY/N;
-        double picketFenceX=(maxX+stepX)/maxX;
-        double picketFenceY=(maxY+stepY)/maxY;
-        double offsetX=(double)stepX/2;
-        double offsetY=(double)stepY/2;
-        
-		double wcount= (double) 1/x.size();
-		int key; 
-		double rndX, rndY;
-		Double tmp;
-		System.out.print("scale="+scaleX + " range=" + rangeX +
-				" min= " + minX + " max= "+ maxX);
-		System.out.println(" wcount= "+wcount);
-		for(int i=0;i<x.size();i++){
+		// round(normalized*Nbins) domain //TODO: ensure capacity ??
+		for(int i=0;i<samples;i++){
 			//TODO: fix picket fence issues
-			rndX= Math.round((((x.get(i)-minX)*picketFenceX)-offsetX)*scaleX);
-			rndY= Math.round((((y.get(i)-minY)*picketFenceY)-offsetY)*scaleY);
-			nX= (int) rndX;
-			nY= (int) rndY;
-			//Map subscript to absolute index
-			key=N*nX + nY;
-			tmp=xy.get(key);
-			
-			/*
-			System.out.println("x= "+ x.get(i) + " rX=" +rndX 
-					+ " nX= " + nX + " y= "+ y.get(i)+
-					" rY=" + " key= " + key + " tmp=" + tmp);
-			*/
-			
-			if(tmp != null ){
-				xy.put(key,tmp + wcount);
-			}else{
-				xy.put(key,wcount);
+			key=0;
+			for(int j=0;j<M;j++){
+				rnd[j]= (int) Math.round((((x[j][i]-minmax[j][0])*picketFence[j])-offset[j])*scale[j]);//maps a sample to a bin
+				key+= (double) mapDim[j]*rnd[j]; //using integer division to have Nbins*rnd  on all except last dimension
+				System.out.println("x[" + j + "][" + i +"]= " + x[j][i]+
+						" rnd=" + rnd[j]+ " mapDim= " + mapDim[j]);
 			}
-
+			//Map subscript to absolute index
+			System.out.println("key= "+ key);
+			tmp=pdf.get(key);
+			tmp=(tmp == null) ? wcount:tmp+wcount;
+			pdf.put(key,tmp);
 		}
 
-
-		System.out.println("map size=" + xy.size());
-		for(Integer d : xy.keySet()){
-			nY=(int)( (double) d/N);
-			nX=d-(nY*N);
-			System.out.print("xy["+ nX +","+ nY +"]=" + xy.get(d) +"\t");
-			System.out.println("d=" + d);
+		//Print HashMap
+		System.out.println("map size=" + pdf.size());
+		int[] dim=new int[M];
+		for(Integer d : pdf.keySet()){
+			System.out.print("pdf["+d+": ");
+			for(int i=0;i<M;i++){
+				//TODO: fix correction factor for higher dimensions
+				dim[i]=d/mapDim[i] - (i*Nbins);
+				System.out.print(dim[i]);
+				if(i<(M-1))
+					System.out.print(",");
+			}
+			System.out.println("]=" + pdf.get(d) +"\t");
 		}
-
 		return pdf;
 
 	}
@@ -103,19 +99,21 @@ public class PdfEstimator {
 	public static void main(String[] args) {
 
 		//Test the histogram estimation
-		ArrayList<Double> x= new ArrayList<Double>();
 		ArrayList<Double> y= new ArrayList<Double>();
-		double[][] hist;
-		int N=1000, bins=2;
-		for(int n=0;n<N;n++){
-			x.add((double) Math.random());
-			//x.add((double) n);
-			//y.add((double) n);
+		HashMap<Integer,Double> hist;
+		int samples=4, bins=4;
+		int dim=2;
+		double[][] x= new double[dim][samples];
+		for(int n=0;n<samples;n++){
+			//x[0][n]= Math.random();
+			for(int d=0;d<dim;d++)
+				x[d][n]=n;
+			//x[1][n]=n;
 			//y.add(x.get(n));
-			y.add((double) Math.random());
+			//x[1][n]=Math.random();
 		}
 		PdfEstimator pdf=new PdfEstimator();
-		hist=pdf.equipartition(x,y,bins);
+		hist=pdf.equipartition(x,bins);
 	}
 
 }
