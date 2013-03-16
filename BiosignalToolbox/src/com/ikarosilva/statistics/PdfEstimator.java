7fa34bb31aa3fa75aa812d916d6fe82e52e19412
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-03-16 01:36:03 +0000

    fixed picket fence bug

--- BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
+++ BiosignalToolbox/src/com/ikarosilva/statistics/PdfEstimator.java
@@ -24,39 +24,6 @@ public class PdfEstimator {
 
 	}
 
-	public double[][] equipartition(ArrayList<Double> x, int N){
-		Nbins=N;
-		pdf = new double[Nbins][3];
-		int LB=0, UB;
-		Collections.sort(x);
-		Double binWidth=(x.get(x.size()-1)-x.get(0))/Nbins;
-		double index;
-		int checkSum=0;
-		for(int n=0;n<Nbins;n++){
-			index = (n+1)*binWidth;
-			if(n != Nbins){
-				UB=Math.abs(Collections.binarySearch(x,index))-1;
-				//Make sure its stable sorted
-				while(UB>0 && UB < x.size() && 
-				(x.get(UB) == x.get(UB-1) ))
-					UB--;
-			}else{
-				UB=x.size()-1;
-			}			
-			pdf[n][0]=(double) (UB-LB+1)/x.size();
-			checkSum+=(UB-LB+1);
-			pdf[n][1]=x.get(LB);
-			pdf[n][2]=x.get(UB);
-			LB=UB+1;
-		}
-		if(checkSum  != x.size()){
-			System.err.println("PDF estimation error: check Sum= " 
-					+ checkSum + " expected: " 
-					+ x.size() + " \n");
-		}
-		return pdf;
-	}
-
 
 	public double[][] equipartition(ArrayList<Double> x, 
 			ArrayList<Double> y, int N){
@@ -90,22 +57,26 @@ public class PdfEstimator {
         
 		double wcount= (double) 1/x.size();
 		int key; 
+		double rndX, rndY;
 		Double tmp;
-		System.out.println("scale="+scaleX + " range=" + rangeX +
+		System.out.print("scale="+scaleX + " range=" + rangeX +
 				" min= " + minX + " max= "+ maxX);
-		System.out.println("wcount= "+wcount);
+		System.out.println(" wcount= "+wcount);
 		for(int i=0;i<x.size();i++){
 			//TODO: fix picket fence issues
-			nX= (int) Math.round((x.get(i)*picketFenceX-minX-offsetX)*scaleX  );
-			nY= (int) Math.round((y.get(i)*picketFenceY-minY-offsetY)*scaleY  );
+			rndX= Math.round((((x.get(i)-minX)*picketFenceX)-offsetX)*scaleX);
+			rndY= Math.round((((y.get(i)-minY)*picketFenceY)-offsetY)*scaleY);
+			nX= (int) rndX;
+			nY= (int) rndY;
 			//Map subscript to absolute index
 			key=N*nX + nY;
 			tmp=xy.get(key);
 			
-			System.out.println("x= "+ x.get(i) + " nX=" +nX 
-					+ " y= "+ y.get(i)+ " nY=" + nY +
-					" key= " + key + " tmp=" + tmp + "\n");
-
+			/*
+			System.out.println("x= "+ x.get(i) + " rX=" +rndX 
+					+ " nX= " + nX + " y= "+ y.get(i)+
+					" rY=" + " key= " + key + " tmp=" + tmp);
+			*/
 			
 			if(tmp != null ){
 				xy.put(key,tmp + wcount);
@@ -117,13 +88,11 @@ public class PdfEstimator {
 
 
 		System.out.println("map size=" + xy.size());
-		int index=0;
 		for(Integer d : xy.keySet()){
 			nY=(int)( (double) d/N);
 			nX=d-(nY*N);
 			System.out.print("xy["+ nX +","+ nY +"]=" + xy.get(d) +"\t");
 			System.out.println("d=" + d);
-			index++;
 		}
 
 		return pdf;
@@ -137,13 +106,13 @@ public class PdfEstimator {
 		ArrayList<Double> x= new ArrayList<Double>();
 		ArrayList<Double> y= new ArrayList<Double>();
 		double[][] hist;
-		int N=1000, bins=6;
+		int N=1000, bins=2;
 		for(int n=0;n<N;n++){
-			//x.add((double) Math.random());
-			x.add((double) n);
-			y.add((double) n);
+			x.add((double) Math.random());
+			//x.add((double) n);
+			//y.add((double) n);
 			//y.add(x.get(n));
-			//y.add((double) Math.random());
+			y.add((double) Math.random());
 		}
 		PdfEstimator pdf=new PdfEstimator();
 		hist=pdf.equipartition(x,y,bins);
