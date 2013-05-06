4f661f657f7ffb4aaa3743bbf060244d0ab75329
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-05-06 20:58:53 +0000

    cleaning up non-linear processes

--- BiosignalToolbox/src/com/ikarosilva/analysis/nonlinear/NonlinearProcess.java
+++ BiosignalToolbox/src/com/ikarosilva/analysis/nonlinear/NonlinearProcess.java
@@ -28,7 +28,7 @@ public class NonlinearProcess {
 			y[i]=x0*Math.exp((k/a)*(1-Math.exp(-a*i*Ts)));
 		return y;
 	}
-	
+
 	public static double[] wbcControl(double x0, double Ts, int N){
 		double[] y=new double[N];
 		double tau=20; //time delay in days
@@ -40,14 +40,29 @@ public class NonlinearProcess {
 		double dx=0;
 		y[0]=x0;
 		for(int i=1;i<N;i++){
-		    xt = (i*Ts<tau) ? 0 : y[(int) (i-Math.round(tau*Ts))];
-		    g=(a*xt)/(1-Math.pow(xt,b));
+			xt = (i*Ts<tau) ? 0 : y[(int) (i-Math.round(tau*Ts))];
+			g=(a*xt)/(1-Math.pow(xt,b));
 			dx=g - 0.1*y[i-1];
 			y[i]=dx*Ts + y[i-1];
-		}
-			
+		}	
 		return y;
 	}
-	
-	
+
+	public static double[] Conway(int N) {
+		double[] output= new double[N];
+		int[] a = new int[N];
+		for(int n=0;n<N;n++){
+			if(n<3){
+				a[n]=1;
+				output[n]=(double) 1;
+			}else{
+				a[n]=a[a[n-1]]+ a[n-a[n-1]];
+				output[n]=(double) a[n] - ((double)n)/2;
+			}
+		}
+		return output;
+
+	}
+
+
 }
