59ee437e2ed98589856a0182ca6d04e6d936f632
ikarosilva00@gmail.com <ikarosilva00@gmail.com@01029dea-a094-b58e-9130-3a99ff8d3de4>
2013-05-24 11:30:40 +0000

    adding some more examples

--- BiosignalToolbox/src/com/ikarosilva/analysis/nonlinear/TransferEntropy.java
+++ BiosignalToolbox/src/com/ikarosilva/analysis/nonlinear/TransferEntropy.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 import com.ikarosilva.graphics.Plot;
 import com.ikarosilva.graphics.RecurrencePlot;
 import com.ikarosilva.graphics.ScatterPlot;
+import com.ikarosilva.statistics.General;
 
 public class TransferEntropy {
 
 	
 	public static void main(String[] args) {
-		int N=1000;
+		int N=200;
 		int mid=(int)Math.round(N/2);
-		double[][] data= NonlinearProcess.modulation(N);
+		double[] data=  NonlinearProcess.LactateSimulation(N);
 		double th=Double.MAX_VALUE;
 		int M=3; //Embedding Dimension
 		int[] neighborSize= {1};//;,2,3,4,5,10,20,40,80,150,300,mid-1};
 		double [] v = new double[neighborSize.length];
 		
+		/*
 		EmbeddedModeling model= new EmbeddedModeling(data[1],1,EmbeddedModeling.Norm.MAX);
 		
 		model.setApplyWeight(true);
@@ -26,10 +28,11 @@ public class TransferEntropy {
 		
 		//Plot plt= new Plot("",neighborSize,v);
 		Plot plt2= new Plot("",data[1]);
-		Plot plt3= new Plot("",data[0]);
+		*/
+		Plot plt3= new Plot("",data);
 		
 		//double[][] p=model.prediction;
-		RecurrencePlot.plotDim(data[1],M);
+		//RecurrencePlot.plotDim(data[1],M);
 		
 		
 		System.out.println("done!");
